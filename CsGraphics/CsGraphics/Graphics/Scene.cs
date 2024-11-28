fde03a866270ac3e0361a3d98af41a43947ccd4e
な～ぺん <mail@na-pen.com>
2024-11-28 21:42:28 +0900

    オブジェクトとシーンの概念を追加

---
+++ CsGraphics/CsGraphics/Graphics/Scene.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsGraphics.CsGraphics.Graphics
+{
+    internal class Scene
+    {
+        public Scene() { }
+
+        public List<Object.Object> objects;
+    }
+}
--- CsGraphics/CsGraphics/Object/Object.cs
+++ CsGraphics/CsGraphics/Object/Object.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CsGraphics.CsGraphics.Object
+namespace CsGraphics.Object
 {
     internal class Object
     {
+        public Object() { }
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsVisible { get; set; }
+
+        private Vertex Vertex { get; set; }
     }
 }
