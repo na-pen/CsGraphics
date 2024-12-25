b4510304edf6e5653eb9afda46e43f49a5fa2c07
な～ぺん <mail@na-pen.com>
2024-12-25 18:08:41 +0900

    クラス構造の整理

---
+++ CsGraphics/Object/Camera/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsGraphics.Object.Camera
+{
+    internal class Camera : Object
+    {
+        public Camera(string name) : base(name)
+        {
+
+        }
+    }
+}
