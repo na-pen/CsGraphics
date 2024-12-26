c59e7478006678bbcf1af911a9822d664dd4065c
な～ぺん <mail@na-pen.com>
2024-12-26 09:59:32 +0900

    カスタムコレクションの実装

---
+++ CsGraphics/ObjectList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsGraphics
+{
+    internal class ObjectList<T>
+        where T : Object.Object
+    {
+            private List<T> items = new List<T>();
+
+            public void Add(T item)
+            {
+                items.Add(item);
+            }
+
+            public T this[int index] => items[index];
+
+            public int Count => items.Count;
+        }
+    }
+}
